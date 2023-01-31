d27aa66d8c29574dbd876e218f414bc10fd5b05f
VincentDoingCode <113548490+VincentDoingCode@users.noreply.github.com>
2023-01-30 18:40:32 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,112 @@
+/*Tools: code editor, browser, command line utility, 
+application and server utility, API platform
+*/
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const genres = [
+    {id:1, name: "Pop"},
+    {id:2, name: "Classical"},
+    {id:3, name: "Rap"},
+    {id:4, name: "Hip Hop"}
+];
+
+const date =[
+    {name: "Light-Switch", year:"2022",month:"Janurary"},
+    {name: "Für Elise", year:"1867",month:"April"},
+    {name: "Lucid-Dream", year:"2018",month:"May"},
+    {name: "Mockingbird", year:"2005",month:"April"}
+];
+//=========== ROUTES FOR HTTP GET REQUESTS ==========
+app.get('/',(req,res)=>{
+    res.send("Welcome to the best song app in the world!");
+});
+app.get('/api/genres',(req,res)=>{
+    res.send(genres);
+});
+
+app.get('/api/genres/:id',(req,res)=>{
+    const gen = genres.find(c=> c.id === parseInt(req.params.id));
+    if(!gen){
+        res.status(404).send("The genre with the given ID was not found");
+        return
+    }
+    res.send(gen);
+});
+
+//date route for songs
+
+app.get('/api/date',(req,res)=>{
+    res.send(date);
+})
+app.get('/api/date/:name',(req,res)=>{
+    const gen = date.find(c=> c.name === String(req.params.name));
+    if(!gen){
+        res.status(404).send("The genre with the given ID was not found");
+        return
+    }
+    res.send(gen);
+});
+
+
+//=========== ROUTES FOR HTTP POST REQUESTS ==========
+app.post('/api/genres',(req,res)=>{
+    if(req.body.name.length > 2){
+        const gen={
+            id:genres.length +1,
+            name: req.body.name
+        }
+        genres.push(gen);
+        res.send(gen);
+    }
+    else{
+        res.send("The genre name should have at least 3 characters.");
+    }
+});
+
+
+
+
+//=========== ROUTES FOR HTTP PUT REQUESTS ==========
+
+app.put('/api/genres/:id', (req,res)=>{
+    const gen = genres.find(c => c.id === parseInt(req.params.id));
+    if(!gen){
+        res.status(404).send('The genre with the given ID was not found');
+        return
+    }
+    if(req.body.length > 2){
+        res.status(404).send('name of the character should be at least three, or more characters long');
+        return
+    }
+    const pos = genres.findIndex(c => c.id === parseInt(req.params.id));
+    const put = {
+        id:req.params.id,
+        name:req.body.name
+    };
+    genres[pos] = put;
+    res.status(200).send(put);
+});
+
+
+
+//=========== ROUTES FOR HTTP DELETE REQUESTS ==========
+app.delete('/api/genres/:id', (req,res)=>{
+    const gen = genres.find(c =>c.id === parseInt(req.params.id));
+    if(!gen){
+        res.status(404).send("The genre with the given ID was not found");
+        return
+    }
+    const pos = genres.findIndex(c => c.id === parseInt(req.params.id));
+    genres.splice(pos,1);
+    res.send(genres[pos]);
+});
+
+
+
+app.listen(3000, ()=>{
+    console.log('Listening on port 3000')
+});
